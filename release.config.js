323d35f1962e5137ffdef5e3380be30e010b1b07
John Goodliff <9030780+twocaretcat@users.noreply.github.com>
2025-11-07 17:01:58 -0700

    ci: add semantic release config

---
+++ release.config.js
@@ -0,0 +1,35 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+	branches: ['main'],
+	plugins: [
+		[
+			'@semantic-release/commit-analyzer',
+			{
+				preset: 'conventionalcommits',
+			},
+		],
+		[
+			'@semantic-release/release-notes-generator',
+			{
+				preset: 'conventionalcommits',
+			},
+		],
+		[
+			'@semantic-release/exec',
+			{
+				prepareCmd: 'deno run -A scripts/update-version.ts ${nextRelease.version}',
+				publishCmd: 'deno task build:npm && deno publish --allow-dirty && cd npm && npm publish && cd ..',
+			},
+		],
+		[
+			'@semantic-release/git',
+			{
+				assets: ['deno.json'],
+				message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+			},
+		],
+		'@semantic-release/github',
+	],
+};
